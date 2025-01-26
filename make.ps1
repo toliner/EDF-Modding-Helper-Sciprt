ad2cac22164e4ea5d712cd116031f9a305408abd
toliner <toliner@toliner.dev>
2025-01-26 21:22:22 +0900

    support mission scripts

--- make.ps1
+++ make.ps1
@@ -22,6 +22,7 @@ $outFolderPath = Join-Path -Path (Get-Location) -ChildPath "out"
 $dotEdfFilePath = Join-Path -Path (Get-Location) -ChildPath ".edf"
 $edf6Path = Get-Content -Path $dotEdfFilePath
 $modFolderPath = Join-Path -Path $edf6Path -ChildPath "Mods"
+$sgott = Join-Path -Path (Get-Location) -ChildPath "sgott.exe"
 
 function commandHelp { 
     Get-Help $PSCommandPath 
@@ -29,6 +30,7 @@ function commandHelp {
 
 function commandCompile { 
     compileWeapon
+    compileMission
 }
 
 function commandDeploy {
@@ -38,14 +40,15 @@ function commandDeploy {
             if (-not (Test-Path -Path $destinationPath)) {
                 New-Item -ItemType Directory -Path $destinationPath -Force
             }
-        } else {
+        }
+        else {
             Copy-Item -Path $_.FullName -Destination $destinationPath -Force
         }
     }
 }
 
 function commandBuild { 
-    compileWeapon 
+    commandCompile
     commandDeploy
 }
 
@@ -53,14 +56,77 @@ function compileWeapon {
     $weaponFolderPath = Join-Path -Path (Get-Location) -ChildPath "WEAPON"
     $jsonFiles = Get-ChildItem -Path $weaponFolderPath -Filter *.json -File
     $outFolderPath = Join-Path -Path $outFolderPath -ChildPath "WEAPON"
-    $sgott = Join-Path -Path (Get-Location) -ChildPath "sgott.exe"
+    
     foreach ($jsonFile in $jsonFiles) {
         $fileName = [System.IO.Path]::GetFileNameWithoutExtension($jsonFile.Name)
         $parts = $fileName -split '-'
         $outFileName = $parts[-1]
         $outFilePath = Join-Path -Path $outFolderPath -ChildPath "$outFileName.sgo"
         $outFile = Get-Item -Path $outFilePath -ErrorAction SilentlyContinue
-        if ($outFile.LastWriteTime -lt $jsonFile.LastWriteTime) {
+        if (!$outFile -or ($outFile.LastWriteTime -lt $jsonFile.LastWriteTime)) {
+            Invoke-Expression "$sgott '$jsonFile' '$outFilePath'"
+        }
+    }
+}
+
+function compileMission {
+    $missionFolderPath = Join-Path -Path (Get-Location) -ChildPath "MISSION"
+    $missionSubFolders = Get-ChildItem -Path $missionFolderPath -Directory -Recurse
+    $rmpaProcessor = Join-Path -Path (Get-Location) -ChildPath "RMPA-Processor.exe"
+
+    # Compile Mission Script Files
+    foreach ($subFolder in $missionSubFolders) {
+        $relativeFolderPath = [System.IO.Path]::GetRelativePath((Get-Location), $subFolder.FullName)
+        $missionOutFolderPath = Join-Path -Path $outFolderPath -ChildPath $relativeFolderPath
+        if (-not (Test-Path -Path $missionOutFolderPath)) {
+            New-Item -ItemType Directory -Path $missionOutFolderPath -Force
+        }
+        $acFile = Get-ChildItem -Path $subFolder.FullName -Filter "MISSION.AC" -File -ErrorAction SilentlyContinue
+        $jsonFile = Get-ChildItem -Path $subFolder.FullName -File | Where-Object { $_.Name -match '^MISSION\.json$' }
+        $rmpaFile = Get-ChildItem -Path $subFolder.FullName -File | Where-Object { $_.Name -match '^RMPA\.json$' }
+            
+        if ($acFile) {
+            Write-Host "Processing $acFile"
+            $destinationPath = Join-Path -Path $missionOutFolderPath -ChildPath "MISSION.AC"
+            $outFile = Get-Item -Path $destinationPath -ErrorAction SilentlyContinue
+            if (!$outFile -or ($outFile.LastWriteTime -lt $jsonFile.LastWriteTime)) {
+                $destinationDir = [System.IO.Path]::GetDirectoryName($destinationPath)
+                if (-not (Test-Path -Path $destinationDir)) {
+                    New-Item -ItemType Directory -Path $destinationDir -Force
+                }
+                Copy-Item -Path $acFile.FullName -Destination $destinationPath -Force
+            }
+        }
+
+        if ($jsonFile) {
+            Write-Host "Processing $jsonFile"
+            $destinationPath = Join-Path -Path $missionOutFolderPath -ChildPath "MISSION.json"
+            $outFile = Get-Item -Path $destinationPath -ErrorAction SilentlyContinue
+            if (!$outFile -or ($outFile.LastWriteTime -lt $jsonFile.LastWriteTime)) {
+                Copy-Item -Path $jsonFile.FullName -Destination $destinationPath -Force
+            }
+        }
+
+        if ($rmpaFile) {
+            Write-Host "Processing $rmpaFile"
+            $destinationPath = Join-Path -Path $missionOutFolderPath -ChildPath "MISSION.RMPA"
+            $outFile = Get-Item -Path $destinationPath -ErrorAction SilentlyContinue
+            if (!$outFile -or ($outFile.LastWriteTime -lt $jsonFile.LastWriteTime)) {
+                Invoke-Expression "$rmpaProcessor '$rmpaFile' '$destinationPath'"
+            }
+        }
+    }
+
+    # Compile Mission List & Text Files
+    $jsonFiles = Get-ChildItem -Path $missionFolderPath -Filter *.json -File
+    $missionOutFolderPath = Join-Path -Path $outFolderPath -ChildPath "MISSION"
+    foreach ($jsonFile in $jsonFiles) {
+        $fileName = [System.IO.Path]::GetFileNameWithoutExtension($jsonFile.Name)
+        $parts = $fileName -split '-'
+        $outFileName = $parts[-1]
+        $outFilePath = Join-Path -Path $missionOutFolderPath -ChildPath "$outFileName.sgo"
+        $outFile = Get-Item -Path $outFilePath -ErrorAction SilentlyContinue
+        if (!$outFile -or ($outFile.LastWriteTime -lt $jsonFile.LastWriteTime)) {
             Invoke-Expression "$sgott '$jsonFile' '$outFilePath'"
         }
     }
